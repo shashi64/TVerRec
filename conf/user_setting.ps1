d4883c8428f925aeadb20c78781d7d3dd4f17385
shashi64 <shashiexplorer@gmail.com>
2025-07-22 20:00:43 +0530

    Add files via upload

---
+++ conf/user_setting.ps1
@@ -0,0 +1,22 @@
+##Start Setting Generated from GUI
+$script:downloadBaseDir = "/home/runner/work/TVerDownloads/TVerDownloads/TVerRec/downloads"
+$script:downloadWorkDir = "/home/runner/work/TVerDownloads/TVerDownloads/TVerRec/temp"
+$script:saveBaseDir = "/home/runner/work/TVerDownloads/TVerDownloads/TVerRec/save"
+$script:addSeriesName = $true
+$script:addSeasonName = $true
+$script:addBroadcastDate = $true
+$script:addEpisodeNumber = $true
+$script:removeSpecialNote = $true
+$script:embedSubtitle = $true
+$script:emptyDownloadBaseDir = $true
+$script:preferredLanguage = 'en-US'
+$script:stopSchedule = @{
+	'Mon' = @()
+	'Tue' = @()
+	'Wed' = @()
+	'Thu' = @()
+	'Fri' = @()
+	'Sat' = @()
+	'Sun' = @()
+}
+##End Setting Generated from GUI
